43408b6b5056cf06ee4cf5666641df4cff87e44e
RinboRobin <111953092+RobinnnnnnnnnnnnRinbo@users.noreply.github.com>
2025-08-09 18:31:07 +0700

    Update CartPage.jsx

--- frontend/src/Pages/CartPage.jsx
+++ frontend/src/Pages/CartPage.jsx
@@ -1,10 +1,14 @@
 import React from "react";
-import NavBar from "../Components/NavBar";
+import NavBar from "../components/NavBar";
 
 const CartPage = ({ cartCount, handleAddToCart, setAddToCart }) => {
   return (
-    <div className="h-screen">
+    <div className="h-screen bg-gray-100">
       <NavBar cartCount={cartCount} />
+      <div className="flex flex-col items-center justify-center h-full font-bold text-xl w-full max-w-md text-center">
+        <span className="text-black text-sm">Your cart quantity:</span>
+        <span className="text-black">{cartCount}</span>
+      </div>
     </div>
   );
 };
